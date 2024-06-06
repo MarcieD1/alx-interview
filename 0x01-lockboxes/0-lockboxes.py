5de9d65d5fe582453788962f81431b83e1564706
MarcieD1 <marciatarisai@icloud.com>
2024-06-06 18:49:48 +0200

    Fixed the Pycodestyle

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
+'''
+Locked box challenge
+'''
+
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-
-    keys = set([0])
-    new_keys = set([0])
-
-    while new_keys:
-        box_keys = set()
-        for key in new_keys:
-            box_keys.update(boxes[key])
-
-        new_keys = box_keys - keys
-        keys.update(new_keys)
-
+    '''
+    Check boxes for key to unlock the next box
+    '''
+    if len(boxes[0]) == 0:
+        return (False)
+    keys = {0}
+    size = len(boxes)
+    visited = {0}
+    keys = keys.union(boxes[0])
+    while size > 0:
+        for i in keys:
+            if i in visited:
+                continue
+            keys = keys.union(boxes[i])
+            visited.add(i)
+        size -= 1
     return len(keys) == len(boxes)
+
+
+    # visited = set([0])
+    # locked = set(boxes[0]).difference(set([0]))
+    # while len(locked) > 0:
+    #     idx = locked.pop()
+    #     if not idx or idx >= len(boxes) or idx < 0:
+    #         continue
+    #     if idx not in visited:
+    #         locked = locked.union(boxes[idx])
+    #         visited.add(idx)
+    # return len(boxes) == len(visited)
