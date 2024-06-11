fe70c2fc3ece5209d44bb995bc2ace1d1d1f5963
MarcieD1 <marciatarisai@icloud.com>
2024-06-11 18:47:27 +0200

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
+"""
+minimum operations
+"""
+
+
 def minOperations(n):
-    if n < 2:
+    """
+    a function that returns an integer that is the number of minimum operations
+    """
+    if n > 1:
+        factors = []
+        i = 2
+        while i * i <= n:
+            if n % i:
+                i += 1
+            else:
+                n //= i
+                factors.append(i)
+        if n > 1:
+            factors.append(n)
+        numOperations = 0
+        for n in factors:
+            numOperations += n
+        return numOperations
+    else:
         return 0
-    operations = 0
-    factor = 2
-    while n > 1:
-        while n % factor == 0:
-            operations += factor
-            n //= factor
-        factor += 1
-    return operations
